2a75ec8e55ad3ecfa97b9fbe8300618b4e7c35c1
felipez <tifonzafel@gmail.com>
2020-09-22 16:50:33 +0200

    removed example code

--- properties_server/data.py
+++ properties_server/data.py
@@ -1,26 +1,25 @@
 """Module to test the interface."""
 
-PROPERTY0 = {
+PROPERTIES = [{
     "id": 0, "smile": "O=O",
     "theory_level": "pbe/dzp",
     "geometry": "2\n\nO 0.0 0.0 0.0\nO 0.0 0.0 1.208",
     "values": {"scscore": 1.5, "bulkiness": 2.0}
-}
-
-PROPERTY1 = {
+},
+    {
     "id": 0, "smile": "CC(=O)O",
     "theory_level": "pbe/dzp",
     "geometry": None,
     "values": None
 }
-
+]
 
 JOBS = [
-    {"id": 1034, "property": PROPERTY1,
+    {"id": 1034, "property": PROPERTIES[1],
      "settings": "{}",
      "status": "AVAILABLE",
      "user": None, "schedule_time": None, "completion_time": None, "platform": None},
-    {"id": 33444, "property": PROPERTY0,
+    {"id": 33444, "property": PROPERTIES[0],
      "settings": "{}",
      "status": "DONE",
      "user": "felipez",
@@ -28,46 +27,4 @@ JOBS = [
      "completion_time": 1600781352.866547,
      "platform": 'Linux-5.4.0-47-generic-x86_64-with-glibc2.10'
      }
-]
-
-# Dictionnary which contains the ingredients based on the recipe ID as key.
-INGREDIENTS = {
-    1: [
-        {"name": "potato", "quantity": 10, "unitMeasurement": "UNIT"},
-        {"name": "onion", "quantity": 2, "unitMeasurement": "UNIT"},
-        {"name": "bacon", "quantity": 100, "unitMeasurement": "GRAM"},
-        {"name": "white wine", "quantity": 0.05, "unitMeasurement": "LITER"},
-        {"name": "reblochon AOP", "quantity": 1, "unitMeasurement": "UNIT"},
-    ],
-    2: [
-        {"name": "potato", "quantity": 1000, "unitMeasurement": "GRAM"},
-        {"name": "bacon", "quantity": 200, "unitMeasurement": "GRAM"},
-        {"name": "onion", "quantity": 200, "unitMeasurement": "GRAM"},
-        {"name": "reblochon AOP", "quantity": 1, "unitMeasurement": "UNIT"},
-        {
-            "name": "tablespoon of oil",
-            "quantity": 2,
-            "unitMeasurement": "UNIT",
-        },
-        {"name": "clove of garlic", "quantity": 1, "unitMeasurement": "UNIT"},
-    ],
-    3: [
-        {"name": "potato", "quantity": 1000, "unitMeasurement": "GRAM"},
-        {"name": "smoked bacon", "quantity": 200, "unitMeasurement": "GRAM"},
-        {"name": "onion", "quantity": 2, "unitMeasurement": "UNIT"},
-        {"name": "reblochon AOP", "quantity": 1, "unitMeasurement": "UNIT"},
-        {"name": "fresh cream", "quantity": 0.100, "unitMeasurement": "LITER"},
-        {"name": "butter", "quantity": 40, "unitMeasurement": "GRAM"},
-        {
-            "name": "tablespoon of pepper",
-            "quantity": 1,
-            "unitMeasurement": "UNIT",
-        },
-    ],
-}
-
-RECIPES = [
-    {"id": 1, "name": "Tartiflette by Eric Guelpa", "cookingTime": 15},
-    {"id": 2, "name": "La 'vraie' Tartiflette", "cookingTime": 20},
-    {"id": 3, "name": "Tartiflette by Alain Ducasse", "cookingTime": 25},
 ]
--- properties_server/query_resolvers.py
+++ properties_server/query_resolvers.py
@@ -8,7 +8,7 @@ from typing import Any, Dict, List, Optional
 
 from tartiflette import Resolver
 
-from properties_server.data import INGREDIENTS, JOBS, PROPERTY0, RECIPES
+from properties_server.data import JOBS
 
 
 @Resolver("Query.jobs")
@@ -19,7 +19,7 @@ async def resolver_query_jobs(
     info: "ResolveInfo",
 ) -> List[Dict[str, Any]]:
     """
-    Resolver in charge of returning available jobs.
+    Resolver in charge of returning jobs based on their status.
 
     Parameters
     ----------
@@ -34,20 +34,20 @@ async def resolver_query_jobs(
 
     Returns
     -------
-    The list of all recipes.
+    The list of all jobs with the given status.
     """
     return [x for x in JOBS if x["status"] == args["status"]]
 
 
-@Resolver("Query.recipes")
-async def resolve_query_recipes(
+@Resolver("Query.job")
+async def resolver_query_job(
     parent: Optional[Any],
     args: Dict[str, Any],
     ctx: Dict[str, Any],
     info: "ResolveInfo",
 ) -> List[Dict[str, Any]]:
     """
-    Resolver in charge of returning all recipes.
+    Resolver in charge of returning a single job based on its status.
 
     Parameters
     ----------
@@ -62,58 +62,6 @@ async def resolve_query_recipes(
 
     Returns
     -------
-    The list of all recipes.
+    A single job with the given status
     """
-    print(args)
-    return RECIPES
-
-
-@Resolver("Query.recipe")
-async def resolve_query_recipe(
-    parent: Optional[Any],
-    args: Dict[str, Any],
-    ctx: Dict[str, Any],
-    info: "ResolveInfo",
-) -> Optional[Dict[str, Any]]:
-    """
-    Resolver in charge of returning a recipe depending on the filled in `id`.
-    :param parent: initial value filled in to the engine `execute` method
-    :param args: computed arguments related to the field
-    :param ctx: context filled in at engine initialization
-    :param info: information related to the execution and field resolution
-    :type parent: Optional[Any]
-    :type args: Dict[str, Any]
-    :type ctx: Dict[str, Any]
-    :type info: ResolveInfo
-    :return: a recipe
-    :rtype: Optional[Dict[str, Any]]
-    """
-    for recipe in RECIPES:
-        if recipe["id"] == args["id"]:
-            return recipe
-    return None
-
-
-@Resolver("Recipe.ingredients")
-async def resolve_recipe_ingredients(
-    parent: Optional[Dict[str, Any]],
-    args: Dict[str, Any],
-    ctx: Dict[str, Any],
-    info: "ResolveInfo",
-) -> Optional[List[Dict[str, Any]]]:
-    """
-    Resolver in charge of returning the ingredient list of a recipe.
-    :param parent: the recipe for which to return the ingredients
-    :param args: computed arguments related to the field
-    :param ctx: context filled in at engine initialization
-    :param info: information related to the execution and field resolution
-    :type parent: Optional[Dict[str, Any]]
-    :type args: Dict[str, Any]
-    :type ctx: Dict[str, Any]
-    :type info: ResolveInfo
-    :return: the ingredient list of a recipe
-    :rtype: Optional[List[Dict[str, Any]]]
-    """
-    if parent and parent["id"] in INGREDIENTS:
-        return INGREDIENTS[parent["id"]]
-    return None
+    return next(x for x in JOBS if x["status"] == args["status"])
