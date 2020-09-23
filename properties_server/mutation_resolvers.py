09680f7da371dce589d0b856293de50120c51d6c
felipez <tifonzafel@gmail.com>
2020-09-23 17:25:57 +0200

    add resolver to add jobs

--- properties_server/mutation_resolvers.py
+++ properties_server/mutation_resolvers.py
@@ -10,6 +10,35 @@ from tartiflette import Resolver
 from properties_server.data import JOBS
 
 
+@Resolver("Mutation.createJob")
+async def resolve_mutation_add_job(
+    parent: Optional[Any],
+    args: Dict[str, Any],
+    ctx: Dict[str, Any],
+    info: "ResolveInfo",
+) -> Dict[str, Any]:
+    """
+    Resolver in charge of Creating a new job.
+
+    Parameters
+    ----------
+    paren
+        initial value filled in to the engine `execute` method
+    args
+        computed arguments related to the field
+    ctx
+        context filled in at engine initialization
+    info
+        information related to the execution and field resolution
+
+    Returns
+    -------
+    Update job
+    """
+    job = args["input"]
+    return job
+
+
 @Resolver("Mutation.updateJob")
 async def resolve_mutation_update_job(
     parent: Optional[Any],
