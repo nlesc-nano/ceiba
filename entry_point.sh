810af41284a4f9fad11380122baf7bdc027aec45
felipez <tifonzafel@gmail.com>
2020-11-02 16:51:17 +0100

    run start app with ansible

--- entry_point.sh
+++ entry_point.sh
@@ -4,4 +4,5 @@ if [[ -z "${MONGO_USER}" ]]; then
     MONGO_USER=admin
 fi
 
-insilico-server -m  172.17.02 -u "${MONGO_USER}" -p "${MONGO_PASSWORD}"
+insilico-server -m  172.17.02 -u "${MONGO_USER}" -p "${MONGO_PASSWORD}" &
+wait $!
