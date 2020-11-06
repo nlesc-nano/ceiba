d1afaa82f74a5e81f4288faa60ab339a9cd3fd19
felipez <tifonzafel@gmail.com>
2020-11-06 10:17:44 +0100

    name docker container with the app as insilico

--- provisioning/start_app.sh
+++ provisioning/start_app.sh
@@ -10,4 +10,4 @@ docker pull felipez/insilico:prototype
 
 # Run the containers
 docker run -d -e "MONGO_INITDB_ROOT_USERNAME=admin" -e "MONGO_INITDB_ROOT_PASSWORD=${MONGO_PASSWORD}"  -p 27017-27019:27017-27019 -v ~/mongo_data:/data/db --name mongodbauth mongo:latest
-docker run -d -e "MONGO_PASSWORD=${MONGO_PASSWORD}" -p 8080:8080 felipez/insilico:prototype
+docker run -d -e "MONGO_PASSWORD=${MONGO_PASSWORD}" -p 8080:8080 --name insilico felipez/insilico:prototype
